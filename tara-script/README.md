df726469b93b98276a60ca770f7045daf42382ea
Bhuribhat <Bhuribhat@gmail.com>
2024-04-27 11:01:06 +0700

    add: tara script

---
+++ tara-script/README.md
@@ -0,0 +1,47 @@
+# Access tara
+
+```bash
+ssh -Y  bratanas@tara.nstda.or.th
+```
+
+## Main working directory
+cd /tarafs/data/project/proj0183-ATS/finetune/lanta-finetune
+
+
+## Upload & Download a file
+
+Run this script in your local terminal
+
+```bash
+# Upload
+scp -r <source_path> bratanas@tara.nstda.or.th:<destination_path>
+
+# Download
+scp -r bratanas@tara.nstda.or.th:<destination_path> <source_path>
+```
+
+## Submit a job
+
+A job must be submitted in main work directory
+
+```bash
+sbatch <script_file>.sh
+```
+
+## View jobs status
+
+```bash
+myqueue
+```
+
+## Cancel a job
+
+```bash
+scancel <job_id>
+```
+
+## View output
+
+```bash
+cat slurm-<job_id>.out
+```
---
+++ tara-script/autoLLM.py
@@ -0,0 +1,111 @@
+import os
+import re
+import sys
+import json
+import time
+import torch
+import pandas as pd
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer
+)
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'expandable_segments:True'
+print("Import completed")
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"using {DEVICE} device")
+
+# Read dataset
+df = pd.read_csv('sample_dataset.csv')
+df["body"] = df["body"].str[14:-4]
+print("Load dataset completed")
+
+# Load model
+model_name_or_path = "/tarafs/data/project/proj0183-ATS/finetune/lanta-finetune/SeaLLM-7B-v2"
+
+def load_LLM_and_tokenizer():
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name_or_path,
+        load_in_4bit=True,
+        local_files_only=True,
+        device_map="auto",
+        torch_dtype=torch.bfloat16,
+        use_cache = False
+    )
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name_or_path,
+        padding_side="left",
+        add_eos_token=True,
+        add_bos_token=True,
+    )
+    tokenizer.pad_token = tokenizer.eos_token
+    return model, tokenizer
+
+
+model, tokenizer = load_LLM_and_tokenizer()
+model.config.use_cache = False
+print("Load model and tokenizer completed")
+
+max_new_tokens = 512    # @param {type: "integer"}
+temperature = 0.2       # @param {type: "number"}
+
+# Inference prompt
+SYSTEM_PROMPT = "ให้สรุปข้อความที่ให้"
+ONE_SHOT_PROMPT = """ตัวอย่างผลลัพธ์ (อย่าเอามาตอบ):
+รอกันมา 2 สัปดาห์ บอลสโมสรลีกต่างๆของยุโรปก็กลับมาดวลแข้งกันอีกครั้งในวีกนี้ ช่วงฟีฟ่าเดย์ รอบล่าสุดทิ้งทวนคัดบอลโลก มีประเด็นให้ฮือฮากันหลังจากอิตาลี หนึ่งในทีมอมตะของวงการลูกหนังโลก"""
+
+
+def generate_inference_prompt(
+    question: str,
+    one_shot: str = ONE_SHOT_PROMPT,
+    system_prompt: str = SYSTEM_PROMPT
+) -> str:
+    return f"""<s><|im_start|>system
+{system_prompt.strip()}
+{one_shot.strip()}</s><|im_start|>user
+{question.strip()}</s><|im_start|>assistant
+"""
+
+
+def generate_summarization(text: str):
+    try:
+        batch = tokenizer(
+            text, return_tensors="pt", padding="max_length", 
+            truncation=True, max_length=4096, add_special_tokens=False
+        )
+        with torch.cuda.amp.autocast():
+            output_tokens = model.generate(
+                input_ids=batch["input_ids"].to(DEVICE),
+                max_new_tokens=max_new_tokens,
+                temperature=temperature,
+                top_p=0.9,
+                repetition_penalty=1.1,
+                do_sample=True,
+                use_cache=False
+            )
+        encoded_answer = output_tokens[0][len(batch["input_ids"][0]):]
+        response = tokenizer.decode(encoded_answer, skip_special_tokens=True)
+        return response
+    except:
+        print(f"Fail attempt!")
+        return None
+
+
+# Main inferencing 
+label_dataset = []
+for i, (index, data_point) in enumerate(df.iterrows()):
+    body = data_point["body"]
+    label = data_point["summary"]
+    text = generate_inference_prompt(body)
+    output = generate_summarization(text)
+    data = {
+        "message" : body,
+        "output"  : output
+    }
+    label_dataset.append(data)
+    if i % 10 == 0:
+        print(f"Processed {i} records")
+
+# Save output to json file
+with open("summarization by SeaLLMs-7B-v2.json", "w", encoding='utf8') as file:
+    json.dump(label_dataset, file)
