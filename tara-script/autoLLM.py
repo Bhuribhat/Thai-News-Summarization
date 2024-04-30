d283c13e926e2215377bbcf5847ef09b7abb36d7
Bhuribhat <Bhuribhat@gmail.com>
2024-04-30 10:49:32 +0700

    set ensure ascii

--- tara-script/autoLLM.py
+++ tara-script/autoLLM.py
@@ -119,4 +119,4 @@ for i, (index, data_point) in enumerate(df.iterrows()):
 
 # Save output to json file
 with open(f"{main_dir}/pattern-proj/summarization_by_SeaLLMs-7B-v2.json", "w", encoding='utf8') as file:
-    json.dump(label_dataset, file)
+    json.dump(label_dataset, file, ensure_ascii=False)
